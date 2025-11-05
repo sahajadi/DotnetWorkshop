34f3be8fb3d381e1c38b6cda9621d2c247d1ef7a
Sahajadi <sahajadilohani123@gmail.com>
2025-11-05 11:50:35 +0545

    task 4 completed

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -87,6 +87,37 @@
     //        Console.WriteLine($"string to double: {stringToDouble}");
     //    }
     //}
+
+
+    // TASK4
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int[] favoriteNumbers = { 7, 21, 3, 14, 9 };
+
+    //        Array.Sort(favoriteNumbers);
+
+    //        Array.Reverse(favoriteNumbers);
+
+    //        Console.WriteLine("Numbers in descending order:");
+    //        for (int i = 0; i < favoriteNumbers.Length; i++)
+    //        {
+    //            Console.WriteLine(favoriteNumbers[i]);
+    //        }
+
+    //        int position = Array.IndexOf(favoriteNumbers, 14);
+    //        if (position != -1)
+    //        {
+    //            Console.WriteLine($"The number 14 is at position {position} in the array.");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("The number 14 is not in the array.");
+    //        }
+    //    }
+    //}
+
 }   
 
 
