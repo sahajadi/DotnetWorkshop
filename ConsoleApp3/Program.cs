901e546f94e22107e3be0ee7a774924f524030a6
Sahajadi <sahajadilohani123@gmail.com>
2025-11-05 11:13:48 +0545

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp3
+{
+    //internal class Program
+    //{            // This is task 1
+    //    static void Main(string[] args)
+    //    {
+    //        // Declare and initialize variables
+    //        string userName = "Sahajadi Lohani";
+    //        int luckyNumber = 5;
+
+    //        // Print using string interpolation
+    //        Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
+    //    }
+    //}
+}
