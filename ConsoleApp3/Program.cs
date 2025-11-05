de59407e32d36d6235c2c548f9be28389f22adb7
Sahajadi <sahajadilohani123@gmail.com>
2025-11-05 11:38:11 +0545

    task 2 completed

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,7 +1,8 @@
 ﻿namespace ConsoleApp3
 {
+    // TASK 1
     //internal class Program
-    //{            // This is task 1
+    //{           
     //    static void Main(string[] args)
     //    {
     //        // Declare and initialize variables
@@ -12,4 +13,38 @@
     //        Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
     //    }
     //}
+
+
+    //     TASK 2
+    //    class Circle
+    //    {
+
+    //        public const double PI = 3.14;
+
+
+    //        public static double CalculateArea(double radius)
+    //        {
+    //            return PI * radius * radius;
+    //        }
+
+
+    //        public static double CalculatePerimeter(double radius)
+    //        {
+    //            return 2 * PI * radius;
+    //        }
+    //    }
+
+    //    internal class Program
+    //    {
+    //        static void Main(string[] args)
+    //        {
+
+    //            Console.WriteLine("--- Task 2: Constants ---");
+
+
+    //            double radius = 5;
+    //            Console.WriteLine($"Area of circle: {Circle.CalculateArea(radius)}");
+    //            Console.WriteLine($"Perimeter of circle: {Circle.CalculatePerimeter(radius)}");
+    //        }
+    //    }
 }
